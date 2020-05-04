3c2c721da41ff0917d122b3f031a6fd679b0a357
Stanislav <fantomart@mail.ru>
2020-05-04 10:50:49 +0300

    Update Belarus

--- covidbot.py
+++ covidbot.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+import pandas
 import requests
 from telebot import TeleBot, types
 from datetime import datetime, timedelta
@@ -14,7 +15,8 @@ POINTS = [
     'Беларусь'
 ]
 
-bot = TeleBot(secret.API_KEY)
+bot = TeleBot(os.environ.get("API_KEY"))
+# bot = TeleBot(secret.API_KEY)
 
 def get_point_buttons():
     return [types.KeyboardButton(point) for point in POINTS]
@@ -50,7 +52,7 @@ def build_statistics_message(data, place):
     if datetime.strptime(last_date, '%d.%m.%Y').date() == datetime.today().date():
         header = f"<b>Данные на сегодня ({last_date}) по {place}</b>:\n"
     else:
-        header = f"<b>На сегодня данных еще нет!</b>\n " \
+        header = f"<b>На сегодня данных еще нет!</b>\n" \
                  f"Последние данные по {place} <b>на {last_date}</b>:\n"
 
     main_stats = f"<b>Заболевших</b>: {values['sick']} (+{values['diff_sick']})\n" \
@@ -129,37 +131,51 @@ def handle_message(message):
             reply_markup=get_markup()
         )
     elif point == "беларусь":
-        start_date = datetime(2020, 3, 30)
-        data = requests.get("https://services.arcgis.com/5T5nSi527N4F7luB/arcgis/rest/services/EURO_COVID19_Running_v3_ALT_DS100/FeatureServer/0/query?f=json&where=(DaysSince100%3E%3D0)%20AND%20(WHO_CODE%3D%27BLR%27)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&groupByFieldsForStatistics=DaysSince100%2CADM0_RUS&outStatistics=%5B%7B%22statisticType%22%3A%22sum%22%2C%22onStatisticField%22%3A%22TotalCases%22%2C%22outStatisticFieldName%22%3A%22value%22%7D%5D&resultType=standard&cacheHint=true")
+        date = datetime.today()
+        link = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/{date}.csv"
+
+        data = requests.get(link.format(date=date.strftime('%m-%d-%Y')))
         if data.status_code != 200:
-            bot.send_message(
-                message.chat.id,
-                "Извините, какой-то сбой, не могу получить данные!",
-                parse_mode='html',
-                reply_markup=get_markup()
-            )
-            return
-        else:
-            data = data.json()
+            if data.status_code == 404:
+                date = datetime.today() - timedelta(days=1)
+                data = requests.get(link.format(date=date.strftime('%m-%d-%Y')))
+                if data.status_code != 200:
+                    bot.send_message(
+                        message.chat.id,
+                        "Извините, какой-то сбой, не могу получить данные!",
+                        parse_mode='html',
+                        reply_markup=get_markup()
+                    )
+                    return
 
+        database = pandas.read_csv(link.format(date=date.strftime('%m-%d-%Y')), index_col='Country_Region')
+        sick = database["Confirmed"]["Belarus"]
+        healed = database["Recovered"]["Belarus"]
+        died = database["Deaths"]["Belarus"]
+        result = [{
+            "sick":sick,
+            "healed": healed,
+            "died": died,
+            "date": date.strftime("%d.%m.%Y")
+        }]
 
-        features = data.get("features", [])
-        to_last_day = features[-1].get("attributes", {}).get("DaysSince100", 0)
-        last_date = start_date + timedelta(days=to_last_day)
-
-        if last_date.date() == datetime.today().date():
-            header = f"<b> Данные на сегодня ({last_date.strftime('%d.%m.%Y')}) по Беларуси</b>:\n"
-        else:
-            header = f"<b>На сегодня данных еще нет!</b>\n" \
-                     f"Последние данные по Беларуси <b>на {last_date.strftime('%d.%m.%Y')}</b>:\n"
-        sick_today = features[-1].get("attributes", {}).get("value", 0)
-        diff_sick = sick_today - features[-2].get("attributes", {}).get("value", 0)
-
-        main_stats = f"<b>Заболевших</b>: {sick_today} (+{diff_sick})\n"
+        database_yesterday = pandas.read_csv(
+            link.format(date=(date - timedelta(days=1)).strftime('%m-%d-%Y')),
+            index_col='Country_Region'
+        )
+        result.append(
+            {
+                "sick": database_yesterday["Confirmed"]["Belarus"],
+                "healed": database_yesterday["Recovered"]["Belarus"],
+                "died": database_yesterday["Deaths"]["Belarus"],
+                "date": (date - timedelta(days=1)).strftime('%d.%m.%Y')
+            }
+        )
+        message_text = build_statistics_message(result, "Беларуси")
 
         bot.send_message(
             message.chat.id,
-            header + main_stats,
+            message_text,
             parse_mode='html',
             reply_markup=get_markup()
         )
@@ -171,7 +187,6 @@ def handle_message(message):
             parse_mode='html',
             reply_markup=get_markup()
         )
-
 
 
 bot.polling(none_stop=True)
